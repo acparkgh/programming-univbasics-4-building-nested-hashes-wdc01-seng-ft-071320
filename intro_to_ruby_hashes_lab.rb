04e24e8a4c03d9ada4b2b086f68e4ba300ee7845
Andrew Park <acpark123@gmail.com>
2020-06-08 04:49:32 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -30,7 +30,7 @@ def monopoly_with_third_tier
   
   { :railroads => {
       :pieces => 4,
-      :rent_in_dollars => { one_piece_owned key set to 25,  },
+      :rent_in_dollars => { one_piece_owned key set to 25, two_pieces_owned key set to 50,  },
       :names => {}
     }
   }
